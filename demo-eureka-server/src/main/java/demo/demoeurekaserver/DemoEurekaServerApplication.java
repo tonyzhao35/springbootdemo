95a91c10d4d888e6ae6b871d7fad65fdf067c1bc
tonyzhao35 <bluesky35@sina.com>
2019-02-28 17:23:09 +0800

    commit

---
+++ demo-eureka-server/src/main/java/demo/demoeurekaserver/DemoEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package demo.demoeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEurekaServerApplication.class, args);
+    }
+
+}
