fb6c31c0ed11548121f64227521854cbbaf47b56
tonyzhao35 <bluesky35@sina.com>
2019-03-02 23:16:53 +0800

    commit

--- demo-eureka-order-service/src/main/java/demo/demoeurekaorderservice/service/impl/OrderServiceImpl.java
+++ demo-eureka-order-service/src/main/java/demo/demoeurekaorderservice/service/impl/OrderServiceImpl.java
@@ -3,10 +3,14 @@ package demo.demoeurekaorderservice.service.impl;
 import demo.demoeurekaorderservice.domain.Order;
 import demo.demoeurekaorderservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.ServiceInstanceChooser;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.lang.reflect.Field;
+import java.net.URI;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -18,11 +22,23 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private RestTemplate restTemplate;
 
+//    @Autowired
+//    private LoadBalancerClient loadBalancer;
+
     @Override
     public Order save(String userId, String productId) {
 
+        // 调用方式一
         // 获取商品详情（使用ribbon）
-        Object obj = restTemplate.getForObject("http://product-service/api/v1/product/find?id=" + productId, Object.class);
+        Map<String, Object> obj = restTemplate.getForObject("http://product-service/api/v1/product/find?id=" + productId, Map.class);
+
+        // 调用方式二
+//        ServiceInstance instance = loadBalancer.choose("product-service");
+//        String uri = String.format("http://%s:%s/api/v1/product/find?id=" + productId,instance.getHost(),instance.getPort());
+//        RestTemplate restTemplate = new RestTemplate();
+//        Map<String, Object> obj = restTemplate.getForObject(uri, Map.class);
+
+        // 调用用户服务，主要是获取用户名称，用户级别或者积分信息
 
         System.out.println(obj);
         if(obj != null){
@@ -30,9 +46,12 @@ public class OrderServiceImpl implements OrderService {
             order.setCreateTime(new Date());
             order.setUserId(userId);
             order.setTradeNo(UUID.randomUUID().toString());
-            order.setProductId(getAttributeValue(obj,"id"));
-            order.setProductName(getAttributeValue(obj,"name"));
-            order.setPrice(Integer.parseInt(getAttributeValue(obj,"price")));
+//            order.setProductId(getAttributeValue(obj,"id"));
+//            order.setProductName(getAttributeValue(obj,"name"));
+//            order.setPrice(Integer.parseInt(getAttributeValue(obj,"price")));
+            order.setProductId(obj.get("id").toString());
+            order.setProductName(obj.get("name").toString());
+            order.setPrice(Integer.parseInt(obj.get("price").toString()));
             return order;
         }
         return null;
@@ -54,7 +73,6 @@ public class OrderServiceImpl implements OrderService {
                 return value==null?null:value.toString();
             }
         }
-
         return null;
     }
 }
